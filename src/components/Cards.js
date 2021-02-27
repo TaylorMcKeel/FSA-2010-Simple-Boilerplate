c8a9e8992f23cb89c979efb7bd041d99b2cfe415
Taylor McKeel <josephmckeel1993@gmail.com>
2021-02-27 00:28:36 -0500

    css changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from 'axios'
+import TinderCard from 'react-tinder-card'
+
 
 
 const API_KEY = 'GM4xwbLME39A7fdii1TaLLa7JHIWYwGLCppJz7zoqUjAsVev8kfGGSZ4eXgtJdX-6lwpGwQgUUzy_ffF7Pp3yZcr2qbQobJj06LAPcbEPg7-fITAfS9Hisze7fo2YHYx'
@@ -18,11 +20,13 @@ export class Cards extends React.Component {
     this.state = {
       location: 'NYC',
       categories: '',
-      restaurants: []
+      restaurants: [],
+      next: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.next = this.next.bind(this)
+    this.nextButt = this.nextButt.bind(this)
   }
 
   async componentDidMount() {
@@ -40,7 +44,15 @@ export class Cards extends React.Component {
     console.log(res.data.businesses)
     this.setState({restaurants: res.data.businesses})   
   }
-  next(){
+  next(dir){
+    if(dir === 'left'){
+      console.log(dir)
+      const res = [...this.state.restaurants]
+      res.shift()
+      this.setState({restaurants: res})
+    }
+  }
+  nextButt(){
     const res = [...this.state.restaurants]
     res.shift()
     this.setState({restaurants: res})
@@ -49,31 +61,47 @@ export class Cards extends React.Component {
   render() {
     const { restaurants } = this.state;
     const res = restaurants[0]
-    return (
-      <div>
-        <h1>NEXTaurants</h1>
-        <div>
-          <label for="location">Zip Code:</label>
-          <input type="text" id="location" name="location" onChange={this.handleChange}/>
-          <label for="categories">Category:</label>
-          <input type="text" id="categories" name="categories" onChange={this.handleChange} placeholder='ex: Thai'/>
-          <button onClick={this.handleSubmit}>Search</button>
-        </div>
-        {res ? <div>
+    if(this.state.next === false){
+      return (
+        <div id='main'>
+          <h1>NEXTaurants</h1>
+          <div id='form'>
+            <div >
+              <label for="location">Zip Code:</label>
+              <input type="text" id="location" name="location" onChange={this.handleChange}/>
+              <label for="categories">Category:</label>
+              <input type="text" id="categories" name="categories" onChange={this.handleChange} placeholder='ex: Thai'/>
+            </div>
+            <button id='search' onClick={this.handleSubmit}>Search</button>
+          </div>
+          {res ? <TinderCard className='card' onSwipe={(dir) => this.next(dir)}>
+            <h2>{res.name}</h2>
+            <img src={res.image_url}/>
+            <p>Rating: {res.rating} ({res.review_count} reviews)</p>
+            <p>Price: {res.price}</p>
+            <p>Phone Number: {res.display_phone}</p>
+            <p>Address: {res.location.address1}</p>
+            <p>{res.location.city}, {res.location.state} {res.location.zip_code}</p>
+          </TinderCard> : ''}
+          <div>
+            <button onClick={this.nextButt}>NEXT</button>
+            <button onClick={()=>{this.setState({next:true})}}>GO</button>
+          </div>
+        </div> 
+      )
+
+    }else{
+      return(
+        <div id='main'>
+          <h1>NEXTaurants</h1>
           <h2>{res.name}</h2>
-          <img src={res.image_url}/>
-          <p>Rating: {res.rating} ({res.review_count} reviews)</p>
-          <p>Price: {res.price}</p>
-          <p>Phone Number: {res.display_phone}</p>
-          <p>Address: {res.location.address1}</p>
-          <p>{res.location.city}, {res.location.state} {res.location.zip_code}</p>
-        </div> : ''}
-        <div>
-          <button onClick={this.next}>NEXT</button>
-          <button ><a href='/directions/${res.location.address1}'>GO</a></button>
-        </div>
-      </div>
-    )
+          <div>
+            <button >Get Directions</button>
+            <button ><a href={res.url}>Make a Reservation</a></button>
+          </div>
+        </div> 
+      )
+    }
   }
 }
 

d3100b9de4c48dc9e9975b49dc1765fd58c81069
Taylor McKeel <josephmckeel1993@gmail.com>
2021-07-29 00:20:29 -0400

    heroku changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const {db} = require('./db');
 const init = async () => {
 	try {
 		const PORT = process.env.PORT || 3000;
-		await db.sync()
+		await db.sync({force: true})
 		app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 	} catch (ex) {
 		console.log(ex);

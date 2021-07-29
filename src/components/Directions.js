20c957ade1602d612f3375ae785cdbd75cd0f902
Taylor McKeel <josephmckeel1993@gmail.com>
2021-07-28 20:55:11 -0400

    fixed map

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -49,7 +49,7 @@ export class Directions extends React.Component {
           //  marker= {this.state.selectedMarker}
            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${MAP_API}&v=3.exp&libraries=geometry,drawing,places`}
            loadingElement={<div style={{ height: `100%` }} />}
-           containerElement={<div style={{ height: `400px`, width: `100%` }} />}
+           containerElement={<div style={{ height: `300px`, width: `40%` }} />}
            mapElement={<div style={{ height: `100%` }} />}
         />
        </div>
